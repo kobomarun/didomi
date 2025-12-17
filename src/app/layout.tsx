3c8204a5ba500a2a8e84a1c8252774b97daa3a32
Olufemi Oluoje <kobomarun@gmail.com>
2025-12-17 09:15:14 +0000

    added tawk.to chat to site

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -29,6 +30,19 @@ export default function RootLayout({
         className={`${inter.variable} font-display bg-background-light dark:bg-background-dark text-text-light dark:text-text-dark antialiased`}
       >
         {children}
+        <Script id="tawk-to-script" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/694270e82f8b1d197df13647/1jcloi3dl';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
